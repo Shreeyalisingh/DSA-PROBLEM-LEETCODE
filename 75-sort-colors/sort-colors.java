de01db2f11e6201fcc272c9740665bda54ac86dc
Shreeyali Singh <118111065+Shreeyalisingh@users.noreply.github.com>
2025-07-06 14:31:15 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (67.87%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,36 +1,33 @@
 class Solution {
-    public void sortColors(int[] nums) {
-       ArrayList<Integer>red = new ArrayList <>();
-         ArrayList<Integer>white= new ArrayList <>();
-           ArrayList<Integer>blue= new ArrayList <>();
-for(int i=0;i<nums.length;i++)
-{
-           if(nums[i]==0)
-           red.add(nums[i]);
-           if(nums[i]==1)
-           white.add(nums[i]);
-           if(nums[i]==2)
-           blue.add(nums[i]);
+    public void sortColors(int[] arr) {
+        int mid=0;int low=0; int high= arr.length-1;int temp =0;
+        while(mid<=high)
+        {
+        if(arr[mid]==0)
+        {
+            temp =arr[mid];
+            arr[mid]=arr[low];
+            arr[low]=temp;
+
+            mid++;
+            low++;
+
+        }
+        else if(arr[mid]==1)
+        {
+         mid++;
+           }
+        else
+        {
+            temp =arr[mid];
+            arr[mid]=arr[high];
+            arr[high]=temp;
+
+            
+          high--;
+
+        }
+        }
+
 }
-    int sred = red.size();
-     int swhite = white.size();
-      int sblue = blue.size();
-
-      for(int i=0;i<sred;i++)
-      {
-        nums[i]=red.get(i);
-      }
-      for(int i=sred;i<swhite+sred;i++)
-      {
-        nums[i]=white.get(i-sred);
-      }
-      for(int i=swhite+sred;i<swhite+sred+sblue;i++)
-      {
-        nums[i]=blue.get(i-(swhite+sred));
-      }
-
-        
-       
-        
-    }
 }
