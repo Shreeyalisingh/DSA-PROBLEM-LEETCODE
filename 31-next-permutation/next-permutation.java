d0191d023bd3e54a36c4e97a274e4170ab944f48
Shreeyali Singh <118111065+Shreeyalisingh@users.noreply.github.com>
2025-07-15 23:42:24 +0530

    Time: 2 ms (4.94%) | Memory: 43.3 MB (36.86%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,47 +1,51 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer>list =  new ArrayList <>();
 
-       
-        for (int num : nums) {
-            list.add(num);
+        for(int i:nums)
+        {
+        list.add(i);  
         }
 
-        int index = -1;
+        int index =-1;
 
-      
-        for (int i = list.size() - 2; i >= 0; i--) {
-            if (list.get(i) < list.get(i + 1)) {
-                index = i;
-                break;
+        for(int i= list.size() -2 ; i>=0;i--)
+        {
+            if(list.get(i)<list.get(i+1))
+            {
+            index = i;
+            break;
             }
-        }
+ }   
 
-        if (index == -1) {
-            Collections.reverse(list);
-            for (int i = 0; i < nums.length; i++) {
-                nums[i] = list.get(i);
-            }
-            return;
-        }
+ if(index==-1)
+ {
+Collections.reverse(list);
+for(int i=0;i<nums.length;i++)
+{
+    nums[i] = list.get(i);
+}
+return;
+ }
 
-       
-        for (int i = list.size() - 1; i > index; i--) {
-            if (list.get(i) > list.get(index)) {
-                int temp = list.get(i);
-                list.set(i, list.get(index));
-                list.set(index, temp);
-                break;
-            }
-        }
+for(int i = list.size()-1;i>index;i--)
+{
+    if(list.get(i)>list.get(index))
+    {
+        int temp = list.get(i);
+        list.set(i,list.get(index));
+        list.set(index,temp);
+        break;
+    }
+}
 
-        
-        List<Integer> newlist = list.subList(index + 1, list.size());
-        Collections.sort(newlist);
+List<Integer> newlist = list.subList(index+1,list.size());
+Collections.sort(newlist);
 
-        
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = list.get(i);
-        }
+for(int i=0;i<nums.length;i++)
+{
+    nums[i] = list.get(i);
+}
+
     }
 }
